848c97b26f9285d98e143754d9cb543f371a1d5b
juan-diego <juan.castro.p@utec.edu.pe>
2024-07-14 16:21:25 -0500

    codigo refactorizado

--- alu.v
+++ alu.v
@@ -4,70 +4,37 @@ module alu (
     output reg [31:0] Result,
     output wire [3:0]  ALUFlags
 );
-
 	wire        neg, zero, carry, overflow;
     wire [31:0] condinvb;
     wire [32:0] sum;
 
-    wire        sign_a, sign_b;
-    wire [7:0]  exponent_a, exponent_b;
-    reg [23:0] mantissa_a, mantissa_b;
-
-    reg         sign_result;
-    reg  [7:0]  exponent_result;
-    reg  [24:0] mantissa_result;
-
-    assign sign_a = a[31];
-    assign sign_b = b[31];
-    assign exponent_a = a[30:23];
-    assign exponent_b = b[30:23];
-    
-    integer add_exp_diff;
-
     assign condinvb = ALUControl[0] ? ~b : b;
-
     assign sum = a + condinvb + ALUControl[0];
 
-    always @(*)
-    begin
-        mantissa_a = {1'b1, a[22:0]};
-        mantissa_b = {1'b1, b[22:0]};
-        add_exp_diff = exponent_a - exponent_b;
+    wire [31:0] add_result;
+    wire [31:0] mul_result;
+    
+    fp_add add (
+        .a(a),
+        .b(b),
+        .result(add_result)
+    );
 
-        exponent_result = (add_exp_diff > 0) ? exponent_a :
-                          (add_exp_diff < 0) ? exponent_b :
-                          exponent_a;
-
-        mantissa_a = (exponent_result <= 0)? mantissa_a >> (-add_exp_diff): mantissa_a;
-        mantissa_b = (exponent_result > 0)? mantissa_b >> add_exp_diff : mantissa_b;
-
-        mantissa_result = (sign_a == sign_b) ? mantissa_a + mantissa_b :
-                          (sign_a == 1'b0 && mantissa_a >= mantissa_b) ? mantissa_a - mantissa_b :
-                          (sign_a == 1'b0 && mantissa_a < mantissa_b)  ? mantissa_b - mantissa_a :
-                          (sign_a == 1'b1 && mantissa_b >= mantissa_a) ? mantissa_b - mantissa_a :
-                          (sign_a == 1'b1 && mantissa_b < mantissa_a)  ? mantissa_a - mantissa_b :
-                          mantissa_a - mantissa_b;
-                          
-        sign_result = (sign_a == sign_b) ? sign_a :
-                      (sign_a == 1'b0) ? (mantissa_a >= mantissa_b ? sign_a : sign_b) :
-                      (sign_a == 1'b1) ? (mantissa_b >= mantissa_a ? sign_b : sign_a) :
-                      sign_a;
-
-        if (mantissa_result[24])
-        begin
-            mantissa_result = mantissa_result >> 1;
-            exponent_result = exponent_result + 1;
-        end
-    end
+    fp_mul mul (
+        .a(a),
+        .b(b),
+        .result(mul_result)
+    );
 
     always @(*)
     begin
         casex (ALUControl[2:0])
-            3'b00?: Result = sum;                                                   // ALUControl[1:0] = 00
-            3'b010: Result = a & b;                                                 // ALUControl[1:0] = 10
-            3'b011: Result = a | b;                                                 // ALUControl[1:0] = 11
-            3'b100: Result = {sign_result, exponent_result, mantissa_result[22:0]}; // ALUControl[1:0] = 00
-            default: Result = 32'b0;                                                // Default case
+            3'b00?: Result = sum;        // ALUControl[1:0] = 00
+            3'b010: Result = a & b;      // ALUControl[1:0] = 10
+            3'b011: Result = a | b;      // ALUControl[1:0] = 11
+            3'b100: Result = add_result; // ALUControl[1:0] = 00
+            3'b101: Result = mul_result;
+            default: Result = 32'b0;     // Default case
         endcase
     end
 
@@ -76,4 +43,5 @@ module alu (
     assign carry    = (ALUControl[1] == 1'b0) & sum[32];
     assign overflow = (ALUControl[1] == 1'b0) & ~(a[31] ^ b[31] ^ ALUControl[0]) & (a[31] ^ sum[31]);
     assign ALUFlags = {neg, zero, carry, overflow};
+
 endmodule
